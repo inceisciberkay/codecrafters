997681a598cb9b5d6c6fd90c793805b2ae337fa2
inceisciberkay <inceisciberkay@gmail.com>
2024-12-23 19:30:47 +0300

    upload grep-rust project

---
+++ codecrafters-grep-rust/src/main.rs
@@ -0,0 +1,36 @@
+mod matcher;
+mod pattern;
+
+use matcher::*;
+use pattern::*;
+
+use std::env;
+use std::io;
+use std::process;
+
+fn match_pattern(input: &str, pattern: &str) -> bool {
+    let matcher = Matcher::new();
+    matcher.match_pattern(input, pattern)
+}
+
+// Usage: echo <input_text> | your_program.sh -E <pattern>
+fn main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    eprintln!("Logs from your program will appear here!");
+
+    if env::args().nth(1).unwrap() != "-E" {
+        println!("Expected first argument to be '-E'");
+        process::exit(1);
+    }
+
+    let pattern = env::args().nth(2).unwrap();
+    let mut input_line = String::new();
+
+    io::stdin().read_line(&mut input_line).unwrap();
+
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
+}
